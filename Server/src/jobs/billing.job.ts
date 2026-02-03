7d14357cfd21f5da225ec2bb7974d713625bc35e
shahrukhfiaz <shahrukhfiaz@users.noreply.github.com>
2026-02-03 09:09:03 -0800

    Fix node-cron import type

--- Server/src/jobs/billing.job.ts
+++ Server/src/jobs/billing.job.ts
@@ -1,4 +1,4 @@
-import cron from 'node-cron';
+import * as cron from 'node-cron';
 import { logger } from '../config/logger';
 import { checkAndDisableExpiredAccounts } from '../services/billing.service';
 
